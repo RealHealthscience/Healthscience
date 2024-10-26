c38e4f0ed154d4bae95d1e6f71427ca37803e0b2
RealHealthscience <therealhealthsciencehub@gmail.com>
2024-10-26 12:12:08 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("learnMoreBtn").addEventListener("click", function() {
+    alert("Breast Cancer Awareness: Early detection can save lives. Schedule regular check-ups and support awareness campaigns!");
+});
